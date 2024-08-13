76644b78a44c60ca0d67d0ba0f8a627b53c33738
Iuri <iurii.vinna@gmail.com>
2024-08-13 06:56:30 -0300

    Pequenos ajustes na tela de time

--- src/dao/ConexaoBdSingleton.java
+++ src/dao/ConexaoBdSingleton.java
@@ -8,29 +8,31 @@ public class ConexaoBdSingleton {
 
 	private Connection connection;
 	private static ConexaoBdSingleton instance;
-	
+
 	private ConexaoBdSingleton() {
 		try {
-			
+
 			// Conexao com o BD de Rafael
-			//connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bet-betina-prod", "postgres", "106927");
-			
+			// connection =
+			// DriverManager.getConnection("jdbc:postgresql://localhost:5432/bet-betina-prod",
+			// "postgres", "106927");
+
 			// Conexao com o BD do IFBA
-			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bet-betina-prod", "postgres", "alunoifba");
-			
+//			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bet-betina-prod", "postgres", "alunoifba");
+
 			// Conexao com o BD de Iuri
-			 //connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bet", "postgres", "admin");
-			
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bet", "postgres", "admin");
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	
+
 	public Connection getConexao() {
-        return connection;
-    }
-	
+		return connection;
+	}
+
 	public static ConexaoBdSingleton getInstance() {
 		if (instance == null) {
 			instance = new ConexaoBdSingleton();
