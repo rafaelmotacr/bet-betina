005272d056cc096d39b786d454e6088967936bd6
rafael mota <rafaelmota361@gmail.com>
2024-08-25 22:15:03 -0300

    agora o banco de dados do docker já inicia com partidas de exemplo.

--- init.sql
+++ init.sql
@@ -58,3 +58,43 @@ CREATE TABLE IF NOT EXISTS bid_tb (
     FOREIGN KEY (bet_id) REFERENCES bet_tb (bet_id) ON DELETE CASCADE,
     FOREIGN KEY (match_id) REFERENCES match_tb (match_id) ON DELETE CASCADE
 );
+
+-- times
+
+INSERT INTO team_tb (team_abbreviation, team_name) VALUES
+('FCB', 'Futebol Clube Barcelona'),
+('RMD', 'Real Madrid'),
+('LIV', 'Liverpool FC'),
+('MNC', 'Manchester City'),
+('PSG', 'Paris Saint-Germain'),
+('JUV', 'Juventus FC'),
+('BAY', 'Bayern Munich'),
+('CHE', 'Chelsea FC'),
+('ATM', 'Atlético Madrid'),
+('DOR', 'Borussia Dortmund'),
+('INT', 'Inter Milan'),
+('ACM', 'AC Milan'),
+('TOT', 'Tottenham Hotspur'),
+('ARS', 'Arsenal FC'),
+('MUN', 'Manchester United'),
+('ROM', 'AS Roma'),
+('NAP', 'Napoli'),
+('BEN', 'Benfica'),
+('POR', 'FC Porto'),
+('LIL', 'Lille OSC');
+
+-- Partidas
+
+INSERT INTO match_tb (match_result, match_state, match_home_team, match_away_team, match_odd_home, match_odd_away, match_odd_draw) VALUES
+(NULL, 1, 1, 2, 1.6, 2.8, 3.1),  -- Futebol Clube Barcelona vs Real Madrid
+(NULL, 1, 3, 4, 1.9, 2.4, 3.2),  -- Liverpool FC vs Manchester City
+(NULL, 1, 5, 6, 2.0, 2.6, 3.3),  -- Paris Saint-Germain vs Juventus FC
+(NULL, 1, 7, 8, 1.7, 2.9, 3.0),  -- Bayern Munich vs Chelsea FC
+(NULL, 1, 9, 10, 2.2, 2.5, 3.1),  -- Atlético Madrid vs Borussia Dortmund
+(NULL, 1, 11, 12, 1.8, 2.7, 3.4),  -- Inter Milan vs AC Milan
+(NULL, 1, 13, 14, 2.1, 2.6, 3.2),  -- Tottenham Hotspur vs Arsenal FC
+(NULL, 1, 15, 16, 1.5, 3.0, 3.5),  -- Manchester United vs AS Roma
+(NULL, 1, 17, 18, 1.6, 2.8, 3.0),  -- Napoli vs Benfica
+(NULL, 1, 19, 20, 2.0, 2.7, 3.1);  -- FC Porto vs Lille OSC
+
+
